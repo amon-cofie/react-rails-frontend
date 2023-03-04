18386747d89967fb12d8de5e7fa0072864fa4eeb
amon-cofie <ddd.amon123@gmail.com>
2023-03-04 14:20:25 +0000

    Build frontend

---
+++ src/redux/configureStore.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import greetingsReducer from './greetings/greetings';
+
+const store = configureStore({
+  reducer: { greeting: greetingsReducer },
+});
+
+export default store;
