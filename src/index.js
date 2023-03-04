705b7decb457a1f52cb4bd41b042435ea4d9d3b1
David  Amon-Cofie <david@goyazi.com>
2023-03-04 15:48:31 +0000

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import './index.css';
 import App from './App';
-
-import { Provider } from 'react-redux';
-
-import store from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
